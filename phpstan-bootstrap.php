f23b989ebdacdf6db2af4c3d93c874bbea44a321
Christoph Massmann <c.massmann@vianetz.com>
2021-02-26 10:14:24 +0100

    replaced Magento default autoloader with new one that checks files for existence

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -7,8 +7,20 @@ use PHPStanMagento1\Autoload\Magento\ModuleControllerAutoloader;
 (new ModuleControllerAutoloader('core'))->register();
 (new ModuleControllerAutoloader('community'))->register();
 
-// workaround Magento's use of date phpdoc typehint for string type
-// better would be to implement the typehint to make it appear string type
-class date {}
+/**
+ * We replace the original Varien_Autoload autoloader with a custom one.
+ */
+spl_autoload_register(static function($className) {
+    spl_autoload_unregister([Varien_Autoload::instance(), 'autoload']);
+
+    $classFile = str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $className)));
+    $classFile .= '.php';
+
+    foreach (explode(':', get_include_path()) as $path) {
+        if (\file_exists($path . DIRECTORY_SEPARATOR . $classFile)) {
+            return include $classFile;
+        }
+    }
+}, true, true);
 
 Mage::app();
